985145e4d89c0a60d5fba2eec225fd10806abb42
Ernesto Cardenas <c_tetto@hotmail.com>
2017-10-18 08:51:05 -0600

    Inicializar Proyecto

---
+++ Tictactoe/Tictactoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
